4e55147511c046eedc433eaaa9affed66fb2f0c1
Carlos Ruiz <ceruiz.dev@outlook.com>
2019-12-08 22:48:39 -0500

    Added react hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 import '../assets/styles/components/Categories.scss'
 import '../assets/styles/components/Carousel.scss'
 import '../assets/styles/components/CarouselItem.scss'
 
-
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Lista 1">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Lista 2">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-);
-
+const API = 'https://us-central1-reservas-5f547.cloudfunctions.net/api'
+const App = () => {
+    const initialState = useInitialState(API);
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            <Categories title="Lista 1">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories title="Lista 2">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    );
+}
 export default App;
