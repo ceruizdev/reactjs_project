0e2d7b8bb6e6ff8f6ce92206fb720ca9b9d7cd6d
Carlos Ruiz <ceruiz.dev@outlook.com>
2019-12-15 20:39:01 -0500

    Redux added to project

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,29 +1,23 @@
-import React,{useState,useEffect} from 'react';
-import Header from '../components/Header';
+import React, {useState,useEffect} from 'react';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
-import '../assets/styles/components/Categories.scss'
-import '../assets/styles/components/Carousel.scss'
-import '../assets/styles/components/CarouselItem.scss'
+import '../assets/styles/components/Categories.scss';
+import '../assets/styles/components/Carousel.scss';
+import '../assets/styles/components/CarouselItem.scss';
 
-const API = 'https://us-central1-reservas-5f547.cloudfunctions.net/api'
-const App = () => {
+const API = 'https://us-central1-reservas-5f547.cloudfunctions.net/api';
+const Home = () => {
     const initialState = useInitialState(API);
     return (
-        <div className="App">
-            <Header />
+        <>
             <Search />
             <Categories title="Lista 1">
                 <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
                     <CarouselItem />
                     <CarouselItem />
                     <CarouselItem />
@@ -36,8 +30,7 @@ const App = () => {
                     <CarouselItem />
                 </Carousel>
             </Categories>
-            <Footer />
-        </div>
+        </>
     );
 }
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App'
-ReactDOM.render(<App />, document.getElementById('app'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './routes/App'
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('app')
+);
