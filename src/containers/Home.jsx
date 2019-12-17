6de7b9b2d7d9c48bb2c8922e0142da4f42f86c0d
Pruebastemp1 <pruebastemp1@GLOBAL.LOC>
2019-12-17 11:47:59 -0500

    Consume service fixed

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,23 +10,23 @@ import '../assets/styles/components/Categories.scss';
 import '../assets/styles/components/Carousel.scss';
 import '../assets/styles/components/CarouselItem.scss';
 
-const API = 'https://us-central1-personal-cv-5fd83.cloudfunctions.net/api';
-const Home = () => {
-    const response =  useInitialState(API);
-
-    return response.length === 0 ? <h1>Cargando...</h1> :(
+const baseURL = 'https://us-central1-personal-cv-5fd83.cloudfunctions.net/api';
+const Home = () =>    
+    {
+        const resp = useInitialState(baseURL);
+        console.log(resp)
+    return resp.length === 0 ? <h1>Cargando...</h1> :(
         <>
             <Search />
-            {response.certificates.length > 0 &&
-                <Categories title="Skills">
-                    <Carousel>     
-                        {response.certificates.map((item,i) =>            
-                            <CarouselItem key={i} {...item} />
-                        )}
-                    </Carousel>
-                </Categories>
-            }          
-        </>
+            <Categories title="Skills">
+                <Carousel>
+                    {resp.certificates.map((item,i) => 
+                        <CarouselItem  key={i} {...item}/>
+                    )}
+                </Carousel>
+            </Categories>              
+        </>   
     );
 }
+
 export default Home;
